32d20718d25038560a5a183ac7ff5a31cccb437e
Rui Batista <ruiandrebatista@gmail.com>
2012-01-17 23:38:44 +0000

    Updated php client example

--- clients/php/lastsnapshot.php
+++ clients/php/lastsnapshot.php
@@ -1,6 +1,6 @@
 <?php
 // Change the url as appropriate.
-$url = "http://localhost:8080/LastSnapshot/main";
+$url = "http://pt.nvda-community.org/ws/LastSnapshot/main";
 $snapshot = json_decode(file_get_contents($url), true) or die("Can't access webservice");
 print "Revision: ". $snapshot["revision"]."\n";
 print "Installer link: ".$snapshot["installer"]."\n";
