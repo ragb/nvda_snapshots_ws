6ef1ab0a52851938287291dc4b89526403b84302
Rui Batista <ruiandrebatista@gmail.com>
2012-01-17 00:06:53 +0000

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(name="nvda_update_ws",
+    author="Rui Batista",
+    author_email="ruiandrebatista@gmail.com",
+    packages=find_packages(),
+    install_requires=["web.py"])
